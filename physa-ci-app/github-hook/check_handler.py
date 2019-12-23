1035d130eaf730937f55d04e5af1f09aec584d53
sommersoft <sommersoft@gmail.com>
2019-12-22 22:32:26 -0600

    fix alibis from naming convention corrections

--- physa-ci-app/github-hook/check_handler.py
+++ physa-ci-app/github-hook/check_handler.py
@@ -9,7 +9,7 @@ from azure.storage import queue
 # pylint: disable=import-error
 from __app__.lib import app_client
 
-class GithubClient(app_client.appClient):
+class GithubClient(app_client.AppClient):
     """ Client object to wrap and contain necessary functions and variables
         to interact with the App.
     """
