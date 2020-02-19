847cd57763ac3b2faaee88ed4b21b1c7bf49049a
sommersoft <sommersoft@gmail.com>
2020-02-19 16:51:33 -0600

    testnode-hook/__init__.py: update imports due to 'node_' file moves

--- physa-ci-app/testnode-hook/__init__.py
+++ physa-ci-app/testnode-hook/__init__.py
@@ -5,10 +5,8 @@ import re
 
 import azure.functions as func
 
-from . import node_github, node_registrar, node_db
-
 # pylint: disable=import-error
-from __app__.lib import result
+from __app__.lib import result, node_github, node_registrar, node_db
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
